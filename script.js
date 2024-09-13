6d4f5a59b0af3182d4acf023a0e9a094f37f3ff8
GiorgiAkhalkatsishvili <146560992+GiorgiAkhalkatsishvili@users.noreply.github.com>
2024-09-13 17:26:21 +0400

    .

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
+const tab1 = document.querySelector(".tab1");
+const tab2 = document.querySelector(".tab2");
+const text1 = document.querySelector(".texts")
+const text2 = document.querySelector(".texts2");
 
 
-const listContainer = document.querySelector(".listContainer");
+tab1.addEventListener("click", () => {
+  tab1.classList.add("border-tab");
+  tab2.classList.remove("border-tab");
+  text1.style.display = "block";
+  text2.style.display = "none";
+})
 
-
-const users = [
-  { name: "John", surname: "Doe", age: 25, email: "john.doe@example.com" },
-  { name: "Jane", surname: "Smith", age: 30, email: "jane.smith@example.com" },
-  { name: "Alice", surname: "Johnson", age: 22, email: "alice.johnson@example.com" },
-  { name: "Bob", surname: "Brown", age: 28, email: "bob.brown@example.com" },
-  { name: "Charlie", surname: "Davis", age: 35, email: "charlie.davis@example.com" },
-  { name: "Emily", surname: "Miller", age: 24, email: "emily.miller@example.com" },
-  { name: "Michael", surname: "Wilson", age: 29, email: "michael.wilson@example.com" },
-  { name: "Sophia", surname: "Moore", age: 26, email: "sophia.moore@example.com" },
-  { name: "Liam", surname: "Taylor", age: 31, email: "liam.taylor@example.com" },
-  { name: "Olivia", surname: "Anderson", age: 27, email: "olivia.anderson@example.com" },
-  { name: "James", surname: "Thomas", age: 33, email: "james.thomas@example.com" },
-  { name: "Mia", surname: "Jackson", age: 21, email: "mia.jackson@example.com" },
-  { name: "Ethan", surname: "White", age: 32, email: "ethan.white@example.com" },
-  { name: "Isabella", surname: "Harris", age: 23, email: "isabella.harris@example.com" },
-  { name: "William", surname: "Martin", age: 34, email: "william.martin@example.com" }
-];
-
-users.forEach((person) => {
-  const listElement = document.createElement("li");
-  listElement.textContent = person.name;
-  const surname = document.createElement("p");
-  surname.textContent = person.surname;
-  const age = document.createElement("p");
-  age.textContent = person.age;
-  const email = document.createElement("p");
-  email.textContent = person.email;
-  
-  listElement.appendChild(surname);
-  listElement.appendChild(age);
-  listElement.appendChild(email);
-  listContainer.appendChild(listElement);
+tab2.addEventListener("click", () => {
+  tab2.classList.add("border-tab")
+  tab1.classList.remove("border-tab")
+  text2.style.display = "block";
+  text1.style.display = "none";
 })
